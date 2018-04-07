1787f7e545e2134baabcd084ab8253992366b5e2
jadotis <jadotis@bu.edu>
2018-04-07 17:20:52 -0400

    Image Serving refactoring

--- config/views.py
+++ config/views.py
@@ -19,20 +19,17 @@ def modules(request):
     filePath = import_config('config.json')['config']['muninDir']
     file_Handle = open(filePath, "r")
     machine_Name = file_Handle.readline()
-    print("initial value of machine_name: ", machine_Name)
+
     while machine_Name != "" and machine_Name[0] != "[" and machine_Name[-1] != "]":
         machine_Name = file_Handle.readline()
-        print("machine_Name is: ", machine_Name)
+
     #TODO implement the error parsing here.
     machine = machine_Name.replace("[", "").replace("]", "")
     finalized_Path = path.join(path.join(import_config('config.json')['config']['imageDirectory'].replace("\n",""), machine.replace("\n",""), machine.replace("\n","")))
 
-    print("finalized path is: ", finalized_Path)
-
     files = []
     for(dirpath, dirnames, filenames) in walk(finalized_Path):
         for file in filenames:
-            print(file)
             if 'html' not in file:
                 files.append(file.replace('png', ''))
     return HttpResponse(str(set(files)), content_type="text/json")
