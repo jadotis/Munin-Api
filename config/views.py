152b4363d96994f25ba7bc547733ad9ec14a8ee0
jadotis <jadotis@bu.edu>
2018-04-07 16:51:28 -0400

    Refactoring using Regular expressions

--- config/views.py
+++ config/views.py
@@ -27,8 +27,9 @@ def modules(request):
         print("machine_Name is: ", machine_Name)
     #TODO implement the error parsing here.
     machine = machine_Name.replace("[", "").replace("]", "")
-    finalized_Path = path.join(path.join(import_config('config.json')['config']['imageDirectory'], machine.translate(whitespace)), machine.translate(whitespace))
-
+    finalized_Path = path.join(path.join(import_config('config.json')['config']['imageDirectory'].translate(whitespace), machine.translate(whitespace)), machine.translate(whitespace))
+    testString = "this is a test of a newLine and \n\n\n\whitespace characters\n\n\n\n"
+    print(testString.translate(whitespace))
     print("finalized path is: ", finalized_Path)
 
     files = []
