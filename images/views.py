a7a7ac9dc6e9a47850fd61cc45519fe49fa679ad
jadotis <jadotis@bu.edu>
2018-04-07 17:31:14 -0400

    Updates to the model

--- images/views.py
+++ images/views.py
@@ -15,10 +15,8 @@ def returnAbsolutePathIndex():
     filePath = import_config('config.json')['config']['muninDir']
     file_Handle = open(filePath, "r")
     machine_Name = file_Handle.readline()
-    print("initial value of machine_name: ", machine_Name)
     while machine_Name != "" and machine_Name[0] != "[" and machine_Name[-1] != "]":
         machine_Name = file_Handle.readline()
-        print("machine_Name is: ", machine_Name)
     #TODO implement the error parsing here.
     machine = machine_Name.replace("[", "").replace("]", "")
     finalized_Path = path.join(path.join(import_config('config.json')['config']['imageDirectory'].replace("\n",""), machine.replace("\n",""), machine.replace("\n","")))
